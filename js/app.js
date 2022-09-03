16b627419e6814232265de4db061b0b60900e496
Sajeeb03 <sajeebmuntasir0@gmail.com>
2022-09-03 07:31:41 +0600

    categories listed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const loadNewsCategories = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+        .then(res => res.json())
+        .then(data => displayNewsCategory(data.data.news_category))
+        .catch(error => console.log(error))
+}
+
+const displayNewsCategory = (categories) => {
+    // console.log(categories);
+    const categoriesContainer = document.getElementById("categories-container");
+    categories.forEach(category => {
+        console.log(category.category_name)
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a>${category.category_name}</a>
+        `;
+        categoriesContainer.appendChild(li);
+    });
+}
+loadNewsCategories();
