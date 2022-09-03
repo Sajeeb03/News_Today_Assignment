64095d1bbf10b5093ddb3cbf85639b0b2e5b9cad
Sajeeb03 <sajeebmuntasir0@gmail.com>
2022-09-03 08:06:41 +0600

    on click functions addeded

--- js/app.js
+++ js/app.js
@@ -9,12 +9,26 @@ const displayNewsCategory = (categories) => {
     // console.log(categories);
     const categoriesContainer = document.getElementById("categories-container");
     categories.forEach(category => {
-        console.log(category.category_name)
+        // console.log(category.category_id)
         const li = document.createElement('li');
+        li.classList.add('categories-list')
+        li.addEventListener('click', function () { showThisNews(category.category_id) })
         li.innerHTML = `
         <a>${category.category_name}</a>
         `;
         categoriesContainer.appendChild(li);
     });
 }
+
+const showThisNews = category_id => {
+    // console.log(category_id)
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+        .then(res => res.json())
+        .then(data => showNews(data.data))
+        .catch(error => console.log(error))
+}
+const showNews = newses => {
+    console.log(newses);
+}
+
 loadNewsCategories();
