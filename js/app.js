0c10b8137c53b30e5622cf06feb6536aa6b39883
Sajeeb03 <sajeebmuntasir0@gmail.com>
2022-09-03 15:31:41 +0600

    responsive done

--- js/app.js
+++ js/app.js
@@ -48,31 +48,34 @@ const showNews = newses => {
         div.classList.add('mb-5')
         div.innerHTML = `
         <div class="card lg:card-side bg-white shadow-xl">
-            <figure><img src="${thumbnail_url}" alt="Album"></figure>
+            <figure><img class="" src="${thumbnail_url}" alt="Album"></figure>
             <div class="card-body">
                     <h2 class="card-title">${title}</h2>
                     <p>${details.length > 350 ? details.slice(0, 350) + '...' : details}</p>
-                    <div class="card-actions absolute bottom-2">
-                        <img src="${author.img}" alt="" style="border-radius: 50%;"
-                        class="h-12 w-12 rounded-full p-0">
-                        <div>
-                        <p>${author.name ? author.name : 'No data found'}</p>
-                        <p>${author.published_date ? author.published_date : 'No data found'}</p>
+                    <div class="flex flex-col mt-5 lg:flex-row justify-between">
+                        
+                        <div class="flex gap-1">
+                            <img src="${author.img}" alt="" style="border-radius: 50%;"
+                            class="h-12 w-12 rounded-full p-0">
+                            <p>${author.name ? author.name : 'No data found'} <br>
+                            ${author.published_date ? author.published_date : 'No data found'}</p>
+   
                         </div>
-                        <div class= "flex gap-1 ml-12 mt-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <div class= "flex gap-1 mt-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                        <p>${total_view ? total_view : 'No data found'}</p>
+                            </svg>
+                            <p>${total_view ? total_view : 'No data found'}</p>
+                        </div>
+                        <div class="flex justify-end">
+                            <label for="my-modal-3" class="btn modal-button" onclick ="loadModal('${_id}')"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                            </svg>
+                            </label>
                         </div>
                     </div>
-                    <div class="card-actions absolute right-2 bottom-2">
-                    <label for="my-modal-3" class="btn modal-button" onclick ="loadModal('${_id}')"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                      </svg>
-                      </label>
-                    </div>
+                    
                 </div>
             </div>
         `;
