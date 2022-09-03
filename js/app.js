83caef7d60df494332db6ba65a57343482cec6ee
Sajeeb03 <sajeebmuntasir0@gmail.com>
2022-09-03 09:18:06 +0600

    views part addeded

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ const showNews = newses => {
     newsContainer.textContent = ``;
     newses.forEach(news => {
         console.log(news)
-        const { author, title, thumbnail_url, details } = news;
+        const { author, title, thumbnail_url, details, total_view } = news;
         const div = document.createElement('div');
         div.classList.add('mb-5')
         div.innerHTML = `
@@ -48,6 +48,13 @@ const showNews = newses => {
                         <div>
                         <p>${author.name ? author.name : 'No data found'}</p>
                         <p>${author.published_date ? author.published_date : 'No data found'}</p>
+                        </div>
+                        <div class= "flex gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        <p>${total_view}</p>
                         </div>
                     </div>
                     <div class="card-actions absolute right-2 bottom-2">
