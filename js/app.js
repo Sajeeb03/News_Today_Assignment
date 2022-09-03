aee5b712509460c1c6495bce00518a1b3114af0b
Sajeeb03 <sajeebmuntasir0@gmail.com>
2022-09-03 09:03:19 +0600

    news section addeded

--- js/app.js
+++ js/app.js
@@ -28,7 +28,36 @@ const showThisNews = category_id => {
         .catch(error => console.log(error))
 }
 const showNews = newses => {
-    console.log(newses);
+    // console.log(newses);
+    const newsContainer = document.getElementById("news-container");
+    newsContainer.textContent = ``;
+    newses.forEach(news => {
+        console.log(news)
+        const { author, title, thumbnail_url, details } = news;
+        const div = document.createElement('div');
+        div.classList.add('mb-5')
+        div.innerHTML = `
+        <div class="card lg:card-side bg-white shadow-xl">
+            <figure><img src="${thumbnail_url}" alt="Album"></figure>
+            <div class="card-body">
+                    <h2 class="card-title">${title}</h2>
+                    <p>${details.length > 350 ? details.slice(0, 350) + '...' : details}</p>
+                    <div class="card-actions absolute bottom-2">
+                        <img src="${author.img}" alt="" style="border-radius: 50%;"
+                        class="h-12 w-12 rounded-full p-0">
+                        <div>
+                        <p>${author.name ? author.name : 'No data found'}</p>
+                        <p>${author.published_date ? author.published_date : 'No data found'}</p>
+                        </div>
+                    </div>
+                    <div class="card-actions absolute right-2 bottom-2">
+                        <button class="btn btn-primary">Show More</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        newsContainer.appendChild(div);
+    });
 }
 
 loadNewsCategories();
